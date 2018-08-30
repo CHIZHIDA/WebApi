8fd25f238e3803ce20ec9c685f49d444442dd4a3
CHIZHIDA <chizhida@vip.qq.com>
2018-08-30 17:15:29 +0800

    container、BaseService

---
+++ WebApi.BLL/Container.cs
@@ -0,0 +1,49 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi.BLL
+{
+    public class Container
+    {
+        /// <summary>
+        /// IOC容器
+        /// </summary>
+        public static IContainer container = null;
+
+        /// <summary>
+        /// 获取Dal实例
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static T Resolve<T>()
+        {
+            try
+            {
+                if(container == null)
+                {
+                    Initialise();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("IOC实例化出错" + ex.Message);
+            }
+
+            return Resolve<T>();
+        }
+
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        public static void Initialise()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<logininfoService>().As<IlogininfoServece>().InstancePerLifetimeScope();
+            container = builder.Build();
+        }
+    }
+}
---
+++ WebApi.BLL/logininfoService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.DAL;
+using WebApi.Model;
+
+namespace WebApi.BLL
+{
+    public partial class logininfoService:BaseService<logininfo>,IlogininfoServece
+    {
+        private IlogininfoDAL logininfoDAL = DAL.Container.Resolve<IlogininfoDAL>();
+        public override void SetDal()
+        {
+            Dal = logininfoDAL;
+        }
+    }
+}
